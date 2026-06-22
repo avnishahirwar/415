f21f452554b83485311abb798ef19dda129bab01
AVNISH KUMAR <124866886+avnishahirwar@users.noreply.github.com>
2026-06-23 03:31:33 +0530

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://gtuycqunlihegsbvvsol.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_zFqzEsppitLusrbVtSakQQ_plPLsg-C";
+
+const supabaseClient = supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
